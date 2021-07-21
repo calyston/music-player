97db0c5b7bcda8016286333de24ab02fabcd79e7
Christine <calyston.14@gmail.com>
2021-07-21 14:14:59 +0100

    feat: song and player components created

---
+++ src/components/Song.js
@@ -0,0 +1,15 @@
+//Song Name Artist and Image
+
+import React from 'react';
+
+const Song = () => {
+  return (
+    <div className="song-container">
+      <h1>Image</h1>
+      <h1>Song Name</h1>
+      <h1>Artist</h1>
+    </div>
+  )
+}
+
+export default Song;
