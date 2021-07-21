82883b25542039b58188625cc18141da37dc47bb
Christine <calyston.14@gmail.com>
2021-07-21 14:35:54 +0100

    feat: svg icons added

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,6 +1,13 @@
 //Music Player Controls
 
 import React from 'react';
+import play from '../assets/icons/play.svg';
+// import pause from '../assets/icons/pause.svg';
+import previous from '../assets/icons/previous.svg';
+import next from '../assets/icons/next.svg';
+// import library from '../assets/icons/library.svg';
+// import repeat from '../assets/icons/repeat.svg';
+// import shuffle from '../assets/icons/shuffle.svg';
 
 const Player = () => {
   return (
@@ -10,7 +17,11 @@ const Player = () => {
         <input type="range" />
         <p>End Time</p>
       </div>
-      <div className="play-control"></div>
+      <div className="play-control">
+        <img className="buttons previous" src={previous} alt="previous" />
+        <img className="buttons play" src={play} alt="play" />
+        <img className="buttons next" src={next} alt="next" />
+      </div>
     </div>
   )
 }
