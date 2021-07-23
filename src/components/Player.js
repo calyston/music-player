f3d8fd404aab23d0909bd5ed2b3606edb0d9ce6b
Christine <calyston.14@gmail.com>
2021-07-23 17:21:44 +0100

    feat: end time duration of song added

--- src/components/Player.js
+++ src/components/Player.js
@@ -45,7 +45,7 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
       <div className="time-control">
         <p>{getTime(songInfo.currentTime)}</p>
         <input type="range" />
-        <p>End Time</p>
+        <p>{getTime(songInfo.duration)}</p>
       </div>
       <div className="play-controls">
         {/* <FontAwesomeIcon className="shuffle" size="3x" icon={faRandom} />
@@ -59,7 +59,11 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
         <svg className="buttons next" xmlns="http://www.w3.org/2000/svg" width="3rem" height="3rem" viewBox="0 0 24 24"><path d="M20 22v-20h2v20h-2zm-18 0l16-10-16-10v20z" /></svg>
         <svg className="buttons repeat" xmlns="http://www.w3.org/2000/svg" width="3rem" height="3rem" viewBox="0 0 24 24"><path d="M2 12c0 .999.381 1.902.989 2.604l-1.098.732-.587.392c-.814-1.025-1.304-2.318-1.304-3.728 0-3.313 2.687-6 6-6h9v-3l6 4-6 4v-3h-9c-2.206 0-4 1.794-4 4zm20.696-3.728l-.587.392-1.098.732c.608.702.989 1.605.989 2.604 0 2.206-1.795 4-4 4h-9v-3l-6 4 6 4v-3h9c3.313 0 6-2.687 6-6 0-1.41-.489-2.703-1.304-3.728z" /></svg>
       </div>
-      <audio onTimeUpdate={updateSongTime} ref={audioRef} src={currentSong.audio}></audio>
+      <audio
+        onTimeUpdate={updateSongTime}
+        onLoadedMetadata={updateSongTime}
+        ref={audioRef}
+        src={currentSong.audio}></audio>
     </div>
   )
 }
